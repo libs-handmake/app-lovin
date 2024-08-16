2090dc1d2e4133a543ce874b3c453d68be3a1b05
Hoang <hoangdv@wavez.vn>
2024-08-16 17:41:02 +0700

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,70 +1,17 @@
-@file:Suppress("UnstableApiUsage")
-
-import compose_config.composeImplementations
-import core_configs.coreAppImplementations
-import core_configs.jetpackComponentImplementation
-import firebase_configs.firebaseCoreImplementation
-
-
 plugins {
-    id("kotlin-kapt")
-    id(Plugins.ANDROID_LIBS)
-    id(Plugins.HILT)
-    kotlin("android")
+    alias(libs.plugins.android.library)
+    id(libs.plugins.hoangdv.library.get().pluginId)
+    id(libs.plugins.hoangdv.core.get().pluginId)
+    id(libs.plugins.hoangdv.jetpack.compose.get().pluginId)
+    id(libs.plugins.hoangdv.firebase.core.get().pluginId)
 }
 
 android {
     namespace = "applovin.hoangdv.libs"
-    compileSdk = Configs.TARGET_SDK
-
-    defaultConfig {
-        minSdk = Configs.MIN_SUPPORTED_SDK
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
-            )
-        }
-    }
-
-    buildFeatures {
-        dataBinding = true
-        viewBinding = true
-        compose = true
-        buildConfig = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = PluginsVer.COMPOSE_COMPILER
-    }
-    compileOptions {
-        sourceCompatibility = Configs.JAVA_TARGET
-        targetCompatibility = Configs.JAVA_TARGET
-    }
-    kotlinOptions {
-        jvmTarget = Configs.JVM_TARGET
-    }
 }
 
 dependencies {
     implementation(project(":base:android-common"))
-    coreAppImplementations()
-    jetpackComponentImplementation()
-    composeImplementations()
-    firebaseCoreImplementation()
-    implementation(Deps.SHIMMER_LAYOUT)
-    implementation(Deps.APP_LOVIN)
-
-//    implementation(AppLovinDeps.ADMOB_ADAPTER)
-//    implementation(AppLovinDeps.IRON_SOURCE_ADAPTER)
-//    implementation(AppLovinDeps.MINTEGRAL_ADAPTER)
-}
-
-kapt {
-    correctErrorTypes = true
+    implementation(libs.shimmer)
+    implementation(libs.applovin.sdk)
 }
